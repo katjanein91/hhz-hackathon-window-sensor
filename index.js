ff20933157f60f60b9fda9664ff29a64ced1a3f7
Your name <you@example.com>
2017-12-12 17:07:21 +0000

    Sensor server with dashboard and mqtt publishing

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ var express = require("express")
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var enocean = require('node-enocean')({sensorFilePath:__dirname+"/data/sensors.json",configFilePath:__dirname+"/data/config.json",timeout:60})
-var Button = require('node-enocean-button')
-var Dimmer = require('node-enocean-dimmer')
+var mqtt = require('mqtt')
+var enocean = require('node-enocean')({sensorFilePath:__dirname+"/data/sensors.json",configFilePath:__dirname+"/data/config.json",timeout:10})
+//var Button = require('node-enocean-button')
+//var Dimmer = require('node-enocean-dimmer')
 
 app.use(express.static('webapp'));
 
@@ -12,16 +13,7 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/webapp/index.html');
 });
 
-app.get('/button/:id/:command', function(req, res){
-	var button = new Button( enocean , req.params.id ) 
-	if(req.params.command == "on" ) {             
-		button.A1.click( )
-	}else{
-		button.A0.click( )	
-	}
-	res.send("ok")
-});
-
+/*
 app.get('/dimmer/:id/:command', function(req, res){
   var dimmer = new Dimmer( enocean , req.params.id )
 	dimmer.speed = "01" // dimm very fast
@@ -38,46 +30,26 @@ app.get('/dimmer/:id/:command', function(req, res){
 	}
 	res.send("ok")
 });
+*/
 
-console.log(enocean.eepDesc);
+/* ------------------------- MQTT --------------------------------------------- */
+var client  = mqtt.connect('mqtt://192.168.0.12', {username: "digitalhhz", password:""})
 
+/* ------------------------- Enocean ------------------------------------------ */
 enocean.listen("/dev/ttyUSB0")
 enocean.emitters.push(io)
-//enocean.eepResolvers.push( (eep, data) => {console.log(eep);})
-
-
-
-app.translateRaw = {
-	'window': (d) => {
-		switch(d.raw){
-			case '08': return 0; break
-			case '09': return 1; break;
-			default: return -1;
-		}
-	}
-}
-
-app.updateData = d => {
-	if(d.hasOwnProperty('senderId') && app.sensors.hasOwnProperty(d.senderId)){
-		app.sensors[d.senderId].timestamp = d.timestamp;
-		app.sensors[d.senderId].value = app.translateRaw[d.type](d);
-		return app.sensors[d.senderId];
-	}
-
-	return {id: '', timestamp: '', value: -1, type: '', title: '', desc: ''};
-};
-
 
 enocean.on('known-data', d => {
 	//var m = app.updateData(d);
 	//console.log(m);
 	io.sockets.emit(d.sensor.title, d);
+	client.publish('hhz/125/' + d.sensor.type + '/' + d.sensor.title, JSON.stringify(d))
 });
 
 enocean.on('ready', function(){
 	console.log('ready');
-	app.sensors = enocean.getSensors();
 });
+
 
 enocean.on('all-sensors', d => console.log("all-sensors",d));
 enocean.on('base', d => console.log("base",d));
@@ -99,18 +71,18 @@ enocean.on('unknown-data', (d) => console.log("unknown data", d));
 enocean.on('unknown-teach-in', (d) => console.log("unknown teach in", d));
 
 
+
+/* ------------------------- Getting Ready ------------------------------------------ */
 io.on('connection', function(socket){
-	console.log('');
-	enocean.register(socket);
+	//enocean.register(socket);
+	socket.emit('init', enocean.getSensors());
 });
 
+/*
 io.on('data', function(data){
 	console.log("Data: ", data);
 });
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/webapp/index.html');
-});
+*/
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
