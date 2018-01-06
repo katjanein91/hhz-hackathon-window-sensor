182046cd0f67fe637e13855906e7da10560c992b
Name <you@example.com>
2018-01-06 17:56:54 +0100

    Commented and refactored

--- index.js
+++ index.js
@@ -4,53 +4,31 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mqtt = require('mqtt')
 var enocean = require('node-enocean')({sensorFilePath:__dirname+"/data/sensors.json",configFilePath:__dirname+"/data/config.json",timeout:10})
-//var Button = require('node-enocean-button')
-//var Dimmer = require('node-enocean-dimmer')
 
-app.use(express.static('webapp'));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/webapp/index.html');
-});
-
-/*
-app.get('/dimmer/:id/:command', function(req, res){
-  var dimmer = new Dimmer( enocean , req.params.id )
-	dimmer.speed = "01" // dimm very fast
-	switch( req.params.command ) {
-	case "teach" :
-		dimmer.teach( ) // 
-	break
-	case "off":
-		dimmer.off( )
-	break
-	default:
-		dimmer.setValue( req.params.command )
-	break;
-	}
-	res.send("ok")
-});
-*/
+// Print all supported sensors of node-enocean
+if(process.argv[2] == 'list' ){
+    console.log("List of supported sensors:", enocean.eepDesc);
+    return 0;
+}
 
 /* ------------------------- MQTT --------------------------------------------- */
 var client  = mqtt.connect('mqtt://192.168.0.12', {username: "digitalhhz", password:""})
 
 /* ------------------------- Enocean ------------------------------------------ */
-enocean.listen("/dev/ttyUSB0")
-enocean.emitters.push(io)
 
+// Register web socket as event emitter, forwards all node-enocean events to clients
+//enocean.emitters.push(io)
+
+/*  When data of known sensors is received:
+ *      - emit event with sensor name as event name and raw enocean data as event data
+ *      - publish to mqtt topic hhz/125/<sensor type>/<sensor name> with JSON encoded enocean data
+ */
 enocean.on('known-data', d => {
-	//var m = app.updateData(d);
-	//console.log(m);
 	io.sockets.emit(d.sensor.title, d);
 	client.publish('hhz/125/' + d.sensor.type + '/' + d.sensor.title, JSON.stringify(d))
 });
 
-enocean.on('ready', function(){
-	console.log('ready');
-});
-
-
+// Debug output to discover events issued by sensors
 enocean.on('all-sensors', d => console.log("all-sensors",d));
 enocean.on('base', d => console.log("base",d));
 enocean.on('forget-error', d => console.log("forget-error",d));
@@ -70,20 +48,26 @@ enocean.on('sent-error', d => console.log("sent-error", d));
 enocean.on('unknown-data', (d) => console.log("unknown data", d));
 enocean.on('unknown-teach-in', (d) => console.log("unknown teach in", d));
 
-
+// Listen for sensor data via usb
+enocean.listen("/dev/ttyUSB0")
 
 /* ------------------------- Getting Ready ------------------------------------------ */
+
+// Serve static web content
+app.use(express.static('webapp'));
+
+// Reply index.html from webapp directory to base URL, e.g. http://localhost:3000
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/webapp/index.html');
+});
+
+// On new web socket connection, Send event 'init' with all available sensors
 io.on('connection', function(socket){
 	//enocean.register(socket);
 	socket.emit('init', enocean.getSensors());
 });
 
-/*
-io.on('data', function(data){
-	console.log("Data: ", data);
-});
-*/
-
+// Listen on port 3000 for http connections
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
--- mqtt_subscriber.js
+++ mqtt_subscriber.js
@@ -1,14 +1,30 @@
+// Load MQTT module
 var mqtt = require('mqtt')
-var client  = mqtt.connect('mqtt://192.168.0.12', {username: "digitalhhz", password:""})
+// Load ical module for parsing google calendar
 var ical = require('ical')
         , months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+// Load module to support easier date procressing
 var moment = require('moment')
+// Load mail module to send email notifications
 var nodemailer = require('nodemailer');
+
+/* ---------------------------------- Configuration ---------------------------------------- */
+
+// MQTT Server
+mqttServer = 'mqtt://192.168.0.12';
+mqttUsername = 'digitalhhz';
+mqttPassword = '';
 
 // Delay after latest event to sent the notification
 var notificationDelay = 1000 * 30;
 
+// Room/location to filter event locations
 var location = "125";
+
+/* --------------------------------- Initialization ---------------------------------------- */
+// Create MQTT client and connect to HHZ MQTT broker
+var client  = mqtt.connect(mqttServer, {username: mqttUsername, password: mqttPassword});
+
 var timerCloseNotification = [];
 var timerTemperatureNotification = [];
 var lastState = [];
@@ -31,6 +47,12 @@ var transporter = nodemailer.createTransport({
 	}
 });
 
+/* ----------------------------- Function definitions ------------------------------------ */
+
+/*  Easily send emails to our test mailbox hhz_raum125@gmx.de.
+ *  @subject: Email subject
+ *  @message: Plain text message
+ */
 var sendMail = (subject, message) => {
 	// setup e-mail data with unicode symbols
 	var mailOptions = {
@@ -50,11 +72,20 @@ var sendMail = (subject, message) => {
 	});
 };
 
+/*  Issue notification, mainily used to prepare the email
+ *  and to print a message to console additionally.
+ *  @msg: Text message for notification
+ *  @sensor: node-enocean sensor information from issuing sensor
+ */
 var notify = (msg,sensor) => {
 	console.log(msg, sensor.sensor.title);
 	sendMail("Room 125 Notification:  " + sensor.sensor.type, msg);
 };
 
+/*  Filter latest event of the current day and given location from ical data.
+ *  @data: ical data
+ *  @location: Filter events for only this location
+ */
 var getLatestEventToday = (data,location) => {
 	var roomEvents = [];
 	for (var k in data)
@@ -70,82 +101,178 @@ var getLatestEventToday = (data,location) => {
 	return latestEventToday;
 };
 
+/*  Verify existance of minmum required sensor informaton
+ *  @sensor: sensor data from node-enocean
+ */
 var validSensor = sensor => (sensor.hasOwnProperty("senderId")
-		&& sensor.hasOwnProperty("sensor") && sensor.sensor.hasOwnProperty("type") && sensor.sensor.hasOwnProperty("title")
-		&& sensor.hasOwnProperty("values") && sensor.values[0].hasOwnProperty("value"));
+		&& sensor.hasOwnProperty("sensor") && sensor.sensor.hasOwnProperty("type")
+                && sensor.sensor.hasOwnProperty("title") && sensor.hasOwnProperty("values")
+                && sensor.values[0].hasOwnProperty("value"));
 
-var handleCloseNotification = sensor => {
-	if(validSensor(sensor))
-	{
-		console.log("Valid message");
 
-		// Clear previous timer
-		if(timerCloseNotification.hasOwnProperty(sensor.senderId)){
-			console.log("Clearing timer of", sensor.sensor.title);
-			clearTimeout(timerCloseNotification[sensor.senderId]);
-		}
+/* ----------------------------- Sensor rules / MQTT message handlers ------------------------------------ */
 
-		// Register new Timer for open event
-		if(sensor.values[0].value == 'open'){
-			ical.fromURL('https://calendar.google.com/calendar/ical/9fbtqhp79a3oqvq6v0ur434j2s%40group.calendar.google.com/private-6be31897ac099f4dbe5c28245f966a6b/basic.ics'
-				, {}, function(err, calendar) {
-					console.log("Calendar callback", sensor.senderId);
-					latestEventToday = getLatestEventToday(calendar, location);
+/* Dummy handler as example for further rules
+ */
+var handleDummy = (topic, message) => {
 
-					console.log("Latest event", latestEventToday.end);
-					console.log("Register timer for ", sensor.sensor.title, "in ", Math.max(latestEventToday.end.diff(moment(),'miliseconds',true),0) + notificationDelay, "miliseconds");
-					timerCloseNotification[sensor.senderId] = setTimeout(notify, Math.max(latestEventToday.end.diff(moment()),0) + notificationDelay, "Close the window in room 125 ", sensor);
-				});
-		}
-	}
-	else{
-		console.log("Invalid message");
-	}
+    // Optional, check if correct subtopic is included
+    if(topic.includes("window")){
+
+        // Translate MQTT message in JSON to Javascript object
+        // Required if data from MQTT message is used
+        var sensor = JSON.parse(message);
+
+        // Validate, that all required sensor information are present
+        // Required, external data should be validated to ensure stability
+        if(validSensor(sensor)){
+
+            // Your rule/action
+            // E.g. print sensor id to server screen
+            console.log(sensor.senderId);
+        }
+        // Debug, print all states
+        console.log("Last States", lastState);
+    }
 };
 
-var handleTemperature = sensor => { console.log("Temperature ",sensor);
-	if(validSensor(sensor)){
-		if(sensor.values[0].value < 20 && lastState.includes("open")){
-			timerTemperatureNotification[sensor.senderId] = setTimeout(notify, notificationDelay, "It's cold, close " + sensor.sensor.title + " in room 125", sensor);
-		}
-		else if(sensor.values[0].value < 20){
-			timerTemperatureNotification[sensor.senderId] = setTimeout(notify, notificationDelay, "Windows are closed, but it's still too cold, turn on the heater in room 125", sensor);
-			//notify("Windows are closed, but it's still too cold, turn on the heater", sensor);
-		}
-		else {
-			console.log("Temperature is fine, " + sensor.values[0].value);
-			if(timerTemperatureNotification.hasOwnProperty(sensor.senderId))clearTimeout(timerTemperatureNotification[sensor.senderId]);
+/*  Rule for window sensors:
+ *  1.  If window is open and the last event today has ended
+ *      -> Send notification to close the windows
+ */
+var handleCloseNotification = (topic, message) => {
 
-		}
-	}
+    // Can only handle door or window sensors
+    if(topic.includes("door") || topic.includes("window")){
+
+        var sensor = JSON.parse(message);
+
+        if(validSensor(sensor))
+        {
+            // Clear previous timer to ensure, that for one sensor only one timer is active, s.t. only one/no duplicate notification is sent.
+            if(timerCloseNotification.hasOwnProperty(sensor.senderId)){
+                console.log("Clearing timer of", sensor.sensor.title);
+                clearTimeout(timerCloseNotification[sensor.senderId]);
+            }
+
+            // If window is opened, register new timer for a notification to close the window
+            // else window was closed and everything is fine.
+            if(sensor.values[0].value == 'open'){
+
+                // Request HHZ calendar from Google Calendar
+                ical.fromURL('https://calendar.google.com/calendar/ical/9fbtqhp79a3oqvq6v0ur434j2s%40group.calendar.google.com/private-6be31897ac099f4dbe5c28245f966a6b/basic.ics'
+                        , {},
+                        function(err, calendar) {
+                            console.log("Calendar callback", sensor.senderId);
+
+                            // Get the latest event of the day from the requested calendar
+                            latestEventToday = getLatestEventToday(calendar, location);
+
+                            console.log("Latest event", latestEventToday.end);
+                            console.log("Register timer for ",
+                                    sensor.sensor.title, "in ",
+                                    Math.max(latestEventToday.end.diff(moment(),'miliseconds',true),0) + notificationDelay
+                                    , "miliseconds"
+                                    );
+                            // Register timer to send a notification after end of last event + some delay
+                            timerCloseNotification[sensor.senderId] = setTimeout(
+                                    notify,
+                                    Math.max(latestEventToday.end.diff(moment()),0) + notificationDelay,
+                                    "Close the window in room 125 ",
+                                    sensor
+                                    );
+                        });
+            }
+        }
+        else{
+            console.log("Invalid message");
+        }
+    }
 };
 
-var handleLastState = sensor => { 
+/*  Rules for temperatur sensors:
+ *  2.  If temperatur < 20 and window(s) open
+ *      -> Notification: close window X in room
+ *  3.  If temperatur < 20 and all windows closed
+ *      -> Notification: Turn on heater
+ */
+var handleTemperature = (topic,message) => {
+
+    // Can only handle temperature sensors
+    if(topic.includes("temperature")){
+
+        var sensor = JSON.parse(message);
+        if(validSensor(sensor)){
+
+            // Clear previous notification/timer
+            if(timerTemperatureNotification.hasOwnProperty(sensor.senderId))
+                clearTimeout(timerTemperatureNotification[sensor.senderId]);
+
+            // Rule 2
+            if(sensor.values[0].value < 20 && lastState.includes("open")){
+                timerTemperatureNotification[sensor.senderId] = setTimeout(
+                    notify,
+                    notificationDelay,
+                    "It's cold, close " + sensor.sensor.title + " in room 125",
+                    sensor
+                );
+            }
+            else
+                // Rule 3
+                if(sensor.values[0].value < 20){
+                    timerTemperatureNotification[sensor.senderId] = setTimeout(
+                        notify,
+                        notificationDelay,
+                        "Windows are closed, but it's still too cold, turn on the heater in room 125",
+                        sensor
+                    );
+                }
+                else {
+                    // Default, only print notification to server screen
+                    console.log("Temperature is fine, " + sensor.values[0].value);
+                }
+        }
+    }
+};
+
+/*  Store the value/state of sensors to make the last value/state of a sensor available for other ruls
+ */
+var handleLastState = (topic, message) => {
+
+    // Check if correct subtopic is included
+    if(topic.includes("window")){
+
+        // Translate MQTT message in JSON to Javascript object
+        var sensor = JSON.parse(message);
+
+        // Validate, that all required sensor information are present
 	if(validSensor(sensor)){
+                // Store value/state for this sensor by sensorId
 		lastState[sensor.senderId] = sensor.values[0].value;
 	}
+        // Debug, print all states
 	console.log("Last States", lastState);
+    }
 }
 
+
+/* ----------------- Register handlers --------------------------------------------------- */
+
+// For debug, print all incoming mqtt messages
+client.on('message', function (topic, message) {
+	console.log(message.toString())
+})
+
+// Register rule handlers for incoming MQTT messages
+
+//client.on('message', handleDummy);
+client.on('message', handleCloseNotification);
+client.on('message', handleTemperature);
+client.on('message', handleLastState);
+
+// When connected to MQTT broker,
+// subscribe to MQTT topics window, door and temperature of room 125
 client.on('connect', function () {
 	client.subscribe(['hhz/125/window/+','hhz/125/door/+', 'hhz/125/temperature/+'])
-	//client.publish('hhz/125/window', 'Hello mqtt')
 })
 
-client.on('message', function (topic, message) {
-	// message is Buffer
-	//console.log(message)
-	console.log(message.toString())
-	//console.log(JSON.parse(message))
-	//client.end()
-
-	if(topic.includes("door") || topic.includes("window")) handleCloseNotification(JSON.parse(message));
-})
-
-client.on('message', function(topic, message){
-	if(topic.includes("temperature"))handleTemperature(JSON.parse(message));
-});
-
-client.on('message', function(topic, message){
-	if( topic.includes("window")) handleLastState(JSON.parse(message));
-});
